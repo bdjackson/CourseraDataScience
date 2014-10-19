a8f2cce23d329b468d88b0d65808acaef667de11
bdjackson <bjack3@gmail.com>
2014-10-19 15:54:30 +0200

    clean up and add comments to polutantmean.R

--- RProgramming/Programming_Assignment_1/pollutantmean.R
+++ RProgramming/Programming_Assignment_1/pollutantmean.R
@@ -1,9 +1,15 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  monitor_files = paste(directory, '/', sprintf('%03d', id), '.csv', sep='')
-  master_list <- c()
-  for (mf in monitor_files) {
-    this_df <- read.csv(mf)
-    master_list <- c(master_list, this_df[[pollutant]])
-  }
+  # first, construct the monitor file names from the list of id's
+  monitor_files <- paste(directory, '/', sprintf('%03d', id), '.csv', sep='')
+  # read in each of the data frames from our list of files. This produces a list of data frames
+  df_list <- lapply(monitor_files, read.csv)
+  
+  # create a master list of pollutant values from our monitors.
+  # to do this, we use lapply(df_list, '[[', pollutant), to get a list of vectors.
+  # each vector is the pollutant info for a particular id.
+  # Next, we unclass this list, to get one big vector with all our readouts.
+  master_list <- unlist(lapply(df_list, '[[', pollutant))
+
+  # finally, return the mean, ignoring NA values
   mean(master_list, na.rm=TRUE)
 }
