bed87e7f086494bd1e5cf66c37fc9628068c0ee4
bdjackson <bjack3@gmail.com>
2014-10-24 11:17:33 +0200

    add placeholder for rankinstate

--- RProgramming/Programming_Assignment_3/HospitalRanking.R
+++ RProgramming/Programming_Assignment_3/HospitalRanking.R
@@ -1,3 +1,4 @@
+# ------------------------------------------------------------------------------
 # Function to read the input CSV file, and return a data frame of the outcome
 ReadOutcomeFile <- function() {
         outcome <- read.csv( 'outcome-of-care-measures.csv'
@@ -7,6 +8,7 @@ ReadOutcomeFile <- function() {
         outcome
 }
 
+# ------------------------------------------------------------------------------
 # function to get the list of hospitals from hospital list file
 ReadHospitalDataFile <- function() {
         hospital.data <- read.csv('hospital-data.csv', colClasses = 'character')
@@ -14,20 +16,44 @@ ReadHospitalDataFile <- function() {
         hospital.data
 }
 
+# ------------------------------------------------------------------------------
 GetStateList <- function(hospital.df) {
         hospital.data <- ReadHospitalDataFile()
         levels(hospital.data$State)
 }
 
+# ------------------------------------------------------------------------------
 # given a data frame and a state, get a list of the hospitals in this state
 GetHospitalsInState <- function(hospital.df, state) {
         # get rows in df with state matching the target state
         hospital.df[hospital.df$State == state, ]
 }
 
+# ------------------------------------------------------------------------------
+# Check that state is valid
+CheckValidState <- function(state, state.list) {
+        if (!(state %in% state.list)) {
+                stop('invalid state')
+        }
+}
+
+# ------------------------------------------------------------------------------
+# check outcome is valid
+CheckValidOutcome <- function(outcome) {
+        valid.outcomes <- c('heart attack', 'heart failure', 'pneumonia')
+        if (!(outcome %in% valid.outcomes)) {
+                stop('invalid outcome')
+        }
+}
+
+# ------------------------------------------------------------------------------
 # function which will take a state and an outcome, and find the best hospital
 # in that state for that outcome
 best <- function(state, outcome) {
+        rankhospital(state, outcome, 'best')
+}
+
+rankhospital <- function(state, outcome, num = 'best') {
         # Read outcome data
         # outcome <- ReadOutcomeFile()
 
@@ -35,16 +61,23 @@ best <- function(state, outcome) {
         state.list <- GetStateList(hospital.df)
 
         # Check that state and outcome are valid
-        if (!(state %in% state.list)) {
-                stop('invalid state')
-        }
-        valid.outcomes <- c('heart attack', 'heart failure', 'pneumonia')
-        if (!(outcome %in% valid.outcomes)) {
-                stop('invalid outcome')
-        }
+        CheckValidState(state, state.list)
+        CheckValidOutcome(outcome)
 
         # Get data for hospitals in this state
         hospitals.in.state <- GetHospitalsInState(hospital.df, state)
 
-        ## Return hospital name in that state with lowest 30-day death rate
+        if (num == 'best') {
+                num <- 1
+        }
+        else if (num == 'worst') {
+                num <- ncol(hospitals.in.state)
+        }
+
+        # rank hospitals
+        # TODO do raking
+        ranked.list <- hospitals.in.state[,2]
+
+        # get hospital in position 'num'
+        ranked.list[num]
 }
