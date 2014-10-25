fb92edf19a137c151f44c2102b9fc9a9f1ea6241
bdjackson <bjack3@gmail.com>
2014-10-25 23:10:49 +0200

    update solution

--- RProgramming/Programming_Assignment_3/HospitalRanking.R
+++ RProgramming/Programming_Assignment_3/HospitalRanking.R
@@ -4,8 +4,37 @@ ReadOutcomeFile <- function() {
         outcome <- read.csv( 'outcome-of-care-measures.csv'
                            , colClasses = 'character'
                            )
+
+        # cast columns 11,17,23 as numeric. These are:
+        #   11: Hospital 30-Day Death (Mortality) Rates from Heart Attack
+        #   17: Hospital 30-Day Death (Mortality) Rates from Heart Failure
+        #   23: Hospital 30-Day Death (Mortality) Rates from Pneumonia
         outcome[, 11] <- as.numeric(outcome[, 11])
+        outcome[, 17] <- as.numeric(outcome[, 17])
+        outcome[, 23] <- as.numeric(outcome[, 23])
         outcome
+}
+
+# ------------------------------------------------------------------------------
+PruneUnneededInfoFromOutcomeDataFrame <- function(full.data.frame) {
+        pruned.data.frame <- full.data.frame[ , c( 2  # Hospital Name
+                                                 , 7  # State
+                                                 , 11 # Hospital 30-Day Death (Mortality) Rates from Heart Attack
+                                                 , 17 # Hospital 30-Day Death (Mortality) Rates from Heart Failure
+                                                 , 23 # Hospital 30-Day Death (Mortality) Rates from Pneumonia
+                                                 )
+                                            ]
+
+        # print(names(pruned.data.frame))
+        names(pruned.data.frame) <- c( 'name'
+                                     , 'state'
+                                     , 'heart attack'
+                                     , 'heart failure'
+                                     , 'pneumonia'
+                                     )
+        # print(names(pruned.data.frame))
+
+        pruned.data.frame
 }
 
 # ------------------------------------------------------------------------------
@@ -53,9 +82,11 @@ best <- function(state, outcome) {
         rankhospital(state, outcome, 'best')
 }
 
+# ------------------------------------------------------------------------------
 rankhospital <- function(state, outcome, num = 'best') {
         # Read outcome data
-        # outcome <- ReadOutcomeFile()
+        hospital.data <- ReadOutcomeFile()
+        hospital.data <- PruneUnneededInfoFromOutcomeDataFrame(hospital.data)
 
         hospital.df <- ReadHospitalDataFile()
         state.list <- GetStateList(hospital.df)
@@ -64,20 +95,45 @@ rankhospital <- function(state, outcome, num = 'best') {
         CheckValidState(state, state.list)
         CheckValidOutcome(outcome)
 
-        # Get data for hospitals in this state
-        hospitals.in.state <- GetHospitalsInState(hospital.df, state)
+        # get hospital data in this state only
+        hospital.data.in.state <- hospital.data[hospital.data$state == state,]
 
+        num <- InterpretPosition(num, hospital.data.in.state)
+        if (is.na(num)) {
+                return(NA)
+        }
+
+        ranked.list <- RankHospitalsByOutcome(hospital.data.in.state, outcome)
+
+        # print('ranked list:')
+        # print(ranked.list)
+
+        # get hospital in position 'num'
+        ranked.list[[num,'name']]
+}
+
+# ------------------------------------------------------------------------------
+RankHospitalsByOutcome <- function(hospital.df, outcome) {
+        # prune na results for the target outcome
+        hospital.df <- hospital.df[!is.na(hospital.df[,outcome]), ]
+
+        # First order by name, then by outcome
+        hospital.df <- hospital.df[order(hospital.df[,'name']), ]
+        ranked.list <- hospital.df[order(hospital.df[,outcome]), ]
+}
+
+# ------------------------------------------------------------------------------
+InterpretPosition <- function(num, hospital.df) {
         if (num == 'best') {
                 num <- 1
         }
         else if (num == 'worst') {
-                num <- ncol(hospitals.in.state)
+                num <- ncol(hospital.df)
+                # num <- NCOL(hospitals.in.state)
+        }
+        else if (num > ncol(hospital.df)) {
+                num <- NA
         }
 
-        # rank hospitals
-        # TODO do raking
-        ranked.list <- hospitals.in.state[,2]
-
-        # get hospital in position 'num'
-        ranked.list[num]
+        num
 }
